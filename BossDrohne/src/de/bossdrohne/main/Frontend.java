3bcc6293b828d2ede3ca56c1d6435932e1406f72
omikron <omikron@Admin>
2018-08-09 21:22:21 +0200

    Added buttonLayout

--- BossDrohne/src/de/bossdrohne/main/Frontend.java
+++ BossDrohne/src/de/bossdrohne/main/Frontend.java
@@ -1,33 +1,46 @@
 package de.bossdrohne.main;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 
 public class Frontend extends JFrame {
 
 	private static final long serialVersionUID = -437139051926418759L;
 
-	private JPanel contentPane = new JPanel(null);
-	private JPanel buttons = new JPanel();
-	private JPanel camera = new JPanel();
-	private JPanel sensor = new JPanel();
-	private JPanel rfid = new JPanel();
-	private JPanel raspberry = new JPanel();
-	private JPanel power = new JPanel();
-	private JButton bttnKam = new JButton("Kamera");
-	private JButton bttnSen = new JButton("Sensoren");
-	private JButton bttnRFID = new JButton("RFID");
-	private JButton bttnRas = new JButton("Raspberry Pi");
-	private JButton bttnAkk = new JButton("Akkustand");
+	private JPanel contentPane = new MotionPanel(this);
+	private JPanel buttonsPane;
+	private JPanel cameraPane = new JPanel(null);
+	private JPanel sensorPane = new JPanel(null);
+	private JPanel rfidPane = new JPanel(null);
+	private JPanel raspberryPane = new JPanel(null);
+	private JPanel powerPane = new JPanel(null);
+	private JButton bttnKam;
+	private JButton bttnSen;
+	private JButton bttnRFID;
+	private JButton bttnRas;
+	private JButton bttnAkk;
+	
+	private int screenWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
+	private int screenHeight = Toolkit.getDefaultToolkit().getScreenSize().height - Toolkit.getDefaultToolkit().getScreenInsets(getGraphicsConfiguration()).bottom;
 	
 	
 	public Frontend() {
 		super("BossDrohne");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setContentPane(contentPane);
-		setExtendedState(JFrame.MAXIMIZED_BOTH);
-		setTitle("myBossDrohne Fernsteuerung");
-		setLayout(new BorderLayout());
+		setResizable(false);
+		setUndecorated(true);
+		contentPane.setLayout(null);
+		contentPane.setPreferredSize(new Dimension(screenWidth, screenHeight));
+		setLocation(0, 0);
 		
 		init();
 		
@@ -36,35 +49,76 @@ public class Frontend extends JFrame {
 	}
 	
 	private void init() {
-		initButtons();
-		initCam();
-	}
-	
-	private void initButtons() {
-		//test
-		add(buttons, BorderLayout.PAGE_END);
-		buttons.add(bttnKam);
-		buttons.add(bttnSen);
-		buttons.add(bttnRFID);
-		buttons.add(bttnRas);
-		buttons.add(bttnAkk);
-		buttons.setLayout(new FlowLayout());
-		buttons.setVisible(true);
-	}
-	
-	private void initCam() {
+		buttonsPane = new JPanel(null);
+		buttonsPane.setBounds(0, Double.valueOf(0.9*screenHeight).intValue(), screenWidth, Double.valueOf(0.1*screenHeight).intValue());
+		buttonsPane.setBackground(Color.WHITE);
+		buttonsPane.setOpaque(true);
+		buttonsPane.setVisible(true);
+		contentPane.add(buttonsPane);
 		
-		add(camera, BorderLayout.PAGE_START);
-		camera.setPreferredSize(new java.awt.Dimension(250, 700));
-		camera.setOpaque(true);
-		camera.setBackground(new Color(0,0,0));
-		camera.setVisible(bttnCamPressed());
+		cameraPane.setBounds(0, 0, screenWidth, Double.valueOf(0.9*screenHeight).intValue());
+		cameraPane.setOpaque(true);
+		cameraPane.setBackground(Color.BLACK);
+		cameraPane.setVisible(false);
+		contentPane.add(cameraPane);
 		
-	}
-	
-	private boolean bttnCamPressed() {
+		contentPane.setBackground(Color.GRAY);
 		
-		return true;
+		bttnKam = new JButton("Kamera");
+		bttnKam.setBounds(0, Double.valueOf(0.15*buttonsPane.getHeight()).intValue(), Double.valueOf(0.2*buttonsPane.getWidth()).intValue(), Double.valueOf(0.7*buttonsPane.getHeight()).intValue());
+		bttnKam.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				boolean state = cameraPane.isVisible();
+				cameraPane.setVisible(!state);
+			}
+		});
+		buttonsPane.add(bttnKam);
 		
+		bttnSen = new JButton("Sensoren");
+		bttnSen.setBounds(Double.valueOf(0.2*buttonsPane.getWidth()).intValue(), Double.valueOf(0.15*buttonsPane.getHeight()).intValue(), Double.valueOf(0.2*buttonsPane.getWidth()).intValue(), Double.valueOf(0.7*buttonsPane.getHeight()).intValue());
+		bttnSen.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		buttonsPane.add(bttnSen);
+		
+		bttnRFID = new JButton("RFID");
+		bttnRFID.setBounds(Double.valueOf(0.4*buttonsPane.getWidth()).intValue(), Double.valueOf(0.15*buttonsPane.getHeight()).intValue(), Double.valueOf(0.2*buttonsPane.getWidth()).intValue(), Double.valueOf(0.7*buttonsPane.getHeight()).intValue());
+		bttnRFID.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		buttonsPane.add(bttnRFID);
+		
+		bttnRas = new JButton("Raspberry Pi");
+		bttnRas.setBounds(Double.valueOf(0.6*buttonsPane.getWidth()).intValue(), Double.valueOf(0.15*buttonsPane.getHeight()).intValue(), Double.valueOf(0.2*buttonsPane.getWidth()).intValue(), Double.valueOf(0.7*buttonsPane.getHeight()).intValue());
+		bttnRas.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		buttonsPane.add(bttnRas);
+		
+		bttnAkk = new JButton("Akkustand");
+		bttnAkk.setBounds(Double.valueOf(0.8*buttonsPane.getWidth()).intValue(), Double.valueOf(0.15*buttonsPane.getHeight()).intValue(), Double.valueOf(0.2*buttonsPane.getWidth()).intValue(), Double.valueOf(0.7*buttonsPane.getHeight()).intValue());
+		bttnAkk.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		buttonsPane.add(bttnAkk);
+
 	}
 }

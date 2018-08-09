91882f60459dd0d561fb7a41d37c2c7e2f2eab16
myBossDrone <40035973+myBossDrone@users.noreply.github.com>
2018-08-09 21:53:23 +0200

    Added Taskbar Added functions to buttons

--- BossDrohne/src/de/bossdrohne/main/Frontend.java
+++ BossDrohne/src/de/bossdrohne/main/Frontend.java
@@ -15,18 +15,17 @@ public class Frontend extends JFrame {
 
 	private static final long serialVersionUID = -437139051926418759L;
 
-	private JPanel contentPane = new MotionPanel(this);
+	private JPanel contentPane = new JPanel(null), taskPane = new JPanel(null);
 	private JPanel buttonsPane;
 	private JPanel cameraPane = new JPanel(null);
 	private JPanel sensorPane = new JPanel(null);
 	private JPanel rfidPane = new JPanel(null);
 	private JPanel raspberryPane = new JPanel(null);
 	private JPanel powerPane = new JPanel(null);
-	private JButton bttnKam;
-	private JButton bttnSen;
-	private JButton bttnRFID;
-	private JButton bttnRas;
-	private JButton bttnAkk;
+	
+	private JButton btnClose;
+	
+	private JButton bttnKam, bttnSen, bttnRFID, bttnRas, bttnAkk;
 	
 	private int screenWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
 	private int screenHeight = Toolkit.getDefaultToolkit().getScreenSize().height - Toolkit.getDefaultToolkit().getScreenInsets(getGraphicsConfiguration()).bottom;
@@ -49,6 +48,11 @@ public class Frontend extends JFrame {
 	}
 	
 	private void init() {
+		taskPane.setLayout(null);
+		taskPane.setBounds(0, 0, screenWidth, Double.valueOf(0.05*screenHeight).intValue());
+		taskPane.setBackground(Color.DARK_GRAY);
+		contentPane.add(taskPane);
+		
 		buttonsPane = new JPanel(null);
 		buttonsPane.setBounds(0, Double.valueOf(0.9*screenHeight).intValue(), screenWidth, Double.valueOf(0.1*screenHeight).intValue());
 		buttonsPane.setBackground(Color.WHITE);
@@ -56,11 +60,35 @@ public class Frontend extends JFrame {
 		buttonsPane.setVisible(true);
 		contentPane.add(buttonsPane);
 		
-		cameraPane.setBounds(0, 0, screenWidth, Double.valueOf(0.9*screenHeight).intValue());
+		cameraPane.setBounds(0, Double.valueOf(0.05*screenHeight).intValue(), screenWidth, Double.valueOf(0.9*screenHeight).intValue());
 		cameraPane.setOpaque(true);
 		cameraPane.setBackground(Color.BLACK);
 		cameraPane.setVisible(false);
 		contentPane.add(cameraPane);
+		
+		rfidPane.setBounds(0, Double.valueOf(0.05*screenHeight).intValue(), screenWidth, Double.valueOf(0.9*screenHeight).intValue());
+		rfidPane.setOpaque(true);
+		rfidPane.setBackground(Color.BLUE);
+		rfidPane.setVisible(false);
+		contentPane.add(rfidPane);
+		
+		sensorPane.setBounds(0, Double.valueOf(0.05*screenHeight).intValue(), screenWidth, Double.valueOf(0.9*screenHeight).intValue());
+		sensorPane.setOpaque(true);
+		sensorPane.setBackground(Color.RED);
+		sensorPane.setVisible(false);
+		contentPane.add(sensorPane);
+		
+		powerPane.setBounds(0, Double.valueOf(0.05*screenHeight).intValue(), screenWidth, Double.valueOf(0.9*screenHeight).intValue());
+		powerPane.setOpaque(true);
+		powerPane.setBackground(Color.YELLOW);
+		powerPane.setVisible(false);
+		contentPane.add(powerPane);
+		
+		raspberryPane.setBounds(0, Double.valueOf(0.05*screenHeight).intValue(), screenWidth, Double.valueOf(0.9*screenHeight).intValue());
+		raspberryPane.setOpaque(true);
+		raspberryPane.setBackground(Color.MAGENTA);
+		raspberryPane.setVisible(false);
+		contentPane.add(raspberryPane);
 		
 		contentPane.setBackground(Color.GRAY);
 		
@@ -70,8 +98,15 @@ public class Frontend extends JFrame {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				boolean state = cameraPane.isVisible();
-				cameraPane.setVisible(!state);
+				if(cameraPane.isVisible()) {
+					cameraPane.setVisible(false);
+				} else {
+					rfidPane.setVisible(false);
+					sensorPane.setVisible(false);
+					raspberryPane.setVisible(false);
+					powerPane.setVisible(false);
+					cameraPane.setVisible(true);
+				}
 			}
 		});
 		buttonsPane.add(bttnKam);
@@ -82,7 +117,15 @@ public class Frontend extends JFrame {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
+				if(sensorPane.isVisible()) {
+					sensorPane.setVisible(false);
+				} else {
+					rfidPane.setVisible(false);
+					cameraPane.setVisible(false);
+					raspberryPane.setVisible(false);
+					powerPane.setVisible(false);
+					sensorPane.setVisible(true);
+				}
 			}
 		});
 		buttonsPane.add(bttnSen);
@@ -93,7 +136,15 @@ public class Frontend extends JFrame {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
+				if(rfidPane.isVisible()) {
+					rfidPane.setVisible(false);
+				} else {
+					raspberryPane.setVisible(false);
+					cameraPane.setVisible(false);
+					sensorPane.setVisible(false);
+					powerPane.setVisible(false);
+					rfidPane.setVisible(true);
+				}
 			}
 		});
 		buttonsPane.add(bttnRFID);
@@ -104,7 +155,15 @@ public class Frontend extends JFrame {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
+				if(raspberryPane.isVisible()) {
+					raspberryPane.setVisible(false);
+				} else {
+					rfidPane.setVisible(false);
+					cameraPane.setVisible(false);
+					sensorPane.setVisible(false);
+					powerPane.setVisible(false);
+					raspberryPane.setVisible(true);
+				}
 			}
 		});
 		buttonsPane.add(bttnRas);
@@ -115,10 +174,29 @@ public class Frontend extends JFrame {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
+				if(powerPane.isVisible()) {
+					powerPane.setVisible(false);
+				} else {
+					rfidPane.setVisible(false);
+					cameraPane.setVisible(false);
+					sensorPane.setVisible(false);
+					raspberryPane.setVisible(false);
+					powerPane.setVisible(true);
+				}
 			}
 		});
 		buttonsPane.add(bttnAkk);
-
+		
+		btnClose = new JButton("x");
+		btnClose.setBounds(0, 0, taskPane.getHeight(), taskPane.getHeight());
+		btnClose.setFont(btnClose.getFont().deriveFont(18f));
+		btnClose.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		taskPane.add(btnClose);
 	}
 }
